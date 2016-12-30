695d329ee597c1920f07c6c574758b7a3f87ea54
sliderxxx <sliderxxx@mail.ru>
2016-12-30 14:26:28 +0300

    First commit

---
+++ wp-x.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package Akismet
+ */
+/*
+Plugin Name: WP-x
+Plugin URI: http://sliderxxx.com/wp-x/
+Description: SEO tools for make money
+Version: 0.0.0.1
+Author: sliderxxx
+Author URI: http://sliderxxx.com/
+License: GPLv2 or later
+Text Domain: sliderxxx
+*/
